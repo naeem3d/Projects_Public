fdc70764fd8e9df8b88c4589ece83e7e32918525
Naeem <naeem3d@gmail.com>
2023-08-22 07:26:35 -0400

    add image and frame

---
+++ Second_Project/Second_Project/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Second_Project
+//
+//  Created by naeem alabboodi on 8/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Second_Project/Second_Project/Second_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Second_ProjectApp.swift
+//  Second_Project
+//
+//  Created by naeem alabboodi on 8/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Second_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
