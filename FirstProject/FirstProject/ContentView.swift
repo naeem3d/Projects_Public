536f797e7ef4f4bd34fefdee0b098140d214027b
Naeem <naeem3d@gmail.com>
2023-08-22 07:14:50 -0400

    first Commit

---
+++ FirstProject/FirstProject/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  FirstProject
+//
+//  Created by naeem alabboodi on 8/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirstProject/FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by naeem alabboodi on 8/22/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
